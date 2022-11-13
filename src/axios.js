6a22987fe2c78432d690bf22f132cbb1ea96b106
William Maphanga <williesdk@gmail.com>
2022-11-13 21:43:43 +0200

    removed middleware

--- src/axios.js
+++ src/axios.js
@@ -2,16 +2,16 @@
 import axios from "axios";
 axios.defaults.baseURL = "https://pg5ydjn92i.execute-api.eu-west-1.amazonaws.com/Prod/";
 // axios.defaults.headers["Authorization"] = "bearer " + localStorage.getItem("token");
-axios.interceptors.request.use(
-    config => {
-      const token = localStorage.getItem("token");
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-      return config;
-    },
-    error => Promise.reject(error)
-  );
+// axios.interceptors.request.use(
+//     config => {
+//       const token = localStorage.getItem("token");
+//       if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//       }
+//       return config;
+//     },
+//     error => Promise.reject(error)
+//   );
 // axios.interceptors.response.use(
 //   function (response) {
 //     return response;
